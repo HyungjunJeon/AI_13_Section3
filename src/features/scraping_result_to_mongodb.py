a3192240c7b2e72100bead5e7b0f4469877ac18f
HyungjunJeon <maru105kr@gmail.com>
2022-06-24 14:13:03 +0900

    Scraping 결과 MongoDB에 저장하는 기능 구현

---
+++ src/features/scraping_result_to_mongodb.py
@@ -0,0 +1,32 @@
+import scraping_melon_playlist as scraping
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+def save_mongodb(url):
+    load_dotenv()
+
+    # MONGODB SETUP
+    HOST = 'cluster0.jyiv0.mongodb.net'
+    USER = 'Hyungjun'
+    PASSWORD = os.environ.get('MONGODB_PASSWORD')
+    DATABASE_NAME = 'hyungjunDB'
+    MONGO_URI = f"mongodb+srv://{USER}:{PASSWORD}@{HOST}/{DATABASE_NAME}?retryWrites=true&w=majority"
+
+    client = MongoClient(MONGO_URI)
+
+    database = client[DATABASE_NAME]
+
+    # scraping result -> MongoDB
+    collection = database['project_scraping']
+
+    collection.delete_many({}) # 중복 실행시 데이터 누적 방지
+
+    result = scraping.scraping(url)
+
+    for i in range(len(result['song_names'])):
+        collection.insert_one({
+                "song_names": result['song_names'][i],
+                "artist_names": result['artist_names'][i],
+                "album_names": result['album_names'][i]
+            })
